7e680d0570c9c103dfdf7f56489576a8854095a7
EvonVonda <EvonVonda@outlook.com>
2026-04-07 09:27:20 +0800

    first commit

---
+++ Hmi.Infrastructure/TestInfra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hmi.Core;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hmi.Infrastructure
+{
+    public class TestInfra
+    {
+        public string GetName()
+        {
+            return new Testcore().Name;
+        }
+    }
+}
